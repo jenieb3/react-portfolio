6aaca1da98c22944336011db574eee58991479c7
Jenelle Spio-Garbrah <jenelle.garbrah3@gmail.com>
2023-03-11 16:22:48 +0000

    updated home and header files, installed bootstrap

---
+++ my-portfolio/src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Jenelle Spio-Garbrah</h1>
+      <img src="../assets/profile-pic.png" alt="Photo of Jenelle" />
+      <p>"Everything is designed. Few things are designed well." -- Brian Reed</p>
+      <h2>Welcome to SpioDesigns</h2>
+      <h2>About Me</h2>
+      <p>I am a lover of life and learning. I do enjoy a good challenge that provokes analytical thinking and problem solving. Also, I like interacting with nature as I draw inspiration, from the wonderful world around us.</p>
+    </div>
+  );
+}
+
+export default Home;
