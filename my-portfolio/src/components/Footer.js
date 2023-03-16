dd5572a79092b48f73b8ced81ca2be1c7f8dbc39
Jenelle Spio-Garbrah <jenelle.garbrah3@gmail.com>
2023-03-16 19:24:42 +0000

    added button functionality to hide my details

--- my-portfolio/src/components/Footer.js
+++ my-portfolio/src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Typography, Link, Box } from '@mui/material';
+import { Container, Typography, Button, Box } from '@mui/material';
 import EmailIcon from '@mui/icons-material/Email';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
@@ -10,19 +10,34 @@ function Footer() {
       <Container maxWidth="lg">
         <Typography variant="h6" color="white">Ways to Contact Me</Typography>
         <Typography color="white">
-          <Link href="mailto:jenelle.garbrah3@gmail.com" color="inherit">
-            <EmailIcon /> Email
-          </Link>
+          <Button
+            href="mailto:jenelle.garbrah3@gmail.com"
+            startIcon={<EmailIcon />}
+            variant="outlined"
+            color="inherit"
+          >
+            Email
+          </Button>
         </Typography>
         <Typography color="white">
-          <Link href="https://www.linkedin.com/in/jenelle-spio-garbrah" color="inherit">
-            <LinkedInIcon /> LinkedIn
-          </Link>
+          <Button
+            href="https://www.linkedin.com/in/jenelle-spio-garbrah"
+            startIcon={<LinkedInIcon />}
+            variant="outlined"
+            color="inherit"
+          >
+            LinkedIn
+          </Button>
         </Typography>
         <Typography color="white">
-          <Link href="https://github.com/jenieb3" color="inherit">
-            <GitHubIcon /> GitHub
-          </Link>
+          <Button
+            href="https://github.com/jenieb3"
+            startIcon={<GitHubIcon />}
+            variant="outlined"
+            color="inherit"
+          >
+            GitHub
+          </Button>
         </Typography>
       </Container>
     </Box>
