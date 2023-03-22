6ebc680bb3f6ebc35fde49d0ccc231fbb1871103
Jenelle Spio-Garbrah <jenelle.garbrah3@gmail.com>
2023-03-22 21:39:18 +0000

    Added gh-pages to deploy to Github.

--- my-portfolio/src/components/App.js
+++ my-portfolio/src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 import Home from '../components/Home';
 import Project from './Projects';
 import Contact from '../components/Contact';
@@ -14,9 +15,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Project />} />
         <Route path="/contact" element={<Contact />} />
-        
-        
       </Routes>
+      <Footer />
     </Router>
   );
 }
--- my-portfolio/src/index.js
+++ my-portfolio/src/index.js
@@ -6,11 +6,6 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import Footer from './components/Footer';
 import '@mui/styled-engine-sc';
 
-
-
-
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
